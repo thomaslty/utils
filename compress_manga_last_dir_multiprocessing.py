aab03bf1acc52e4146d88d8fe757abb890e7a0c1
thomaslty <thomaslty16@gmail.com>
2021-11-21 14:42:30 +0000

    add multiprocessing for manga compress

---
+++ compress_manga_last_dir_multiprocessing.py
@@ -0,0 +1,23 @@
+import shutil
+import os
+import sys
+
+def zipit(path):
+    try:
+        shutil.make_archive(path, "zip", path)
+        shutil.rmtree(path)
+    except:
+        print("error on", path)
+
+def get_path_list(input_path):
+    path_list = []
+    for root, dirs, files in os.walk(input_path):
+        if not dirs:
+            path_list.append(root)
+	return path_list
+    
+if __name__ == '__main__':
+    input_path = sys.argv[1]
+    path_list = get_path_list(input_path)
+    pool = Pool()
+    pool.map(zipit, path_list)
