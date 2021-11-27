96ab2de3a7a46a651529040b169d214197011db5
thomaslty <thomaslty16@gmail.com>
2021-11-27 03:57:44 +0000

    update rar file handle

--- convert_7z_to_zip_multiprocessing.py
+++ convert_7z_to_zip_multiprocessing.py
@@ -3,16 +3,21 @@ import os
 import sys
 import patoolib
 from multiprocessing import Pool
+from rarfile import RarFile
 
 def conversion(path):
     try:
         path_noext = path.rsplit(".", 1)[0]
-        patoolib.extract_archive(path, outdir=path_noext)
+        ext = path.rsplit(".", 1)[1]
+        if (ext == "rar"):
+            RarFile(path).extractall(path=path_noext)
+        else:
+            patoolib.extract_archive(path, outdir=path_noext)
         os.remove(path)
         shutil.make_archive(path_noext, "zip", path_noext)
         shutil.rmtree(path_noext)
-    except:
-      print("error on", path)
+    except Exception as e:
+      print("error", e)
 
 def get_path_list(input_path):
     path_list = []
